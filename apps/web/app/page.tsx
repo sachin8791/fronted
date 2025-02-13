bc6271ecb6fbd235984cbe7826ed1742bd557255
Sachin Kumar <sachinkumar879162@gmail.com>
2025-02-13 10:40:49 +0530

    Init commit

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,12 +1,35 @@
-import { Button } from "@workspace/ui/components/button"
+"use client";
+
+import Editor from "@workspace/editor/components/Editor";
+
+interface File {
+  name: string;
+  language: "javascript" | "css" | "html";
+  content: string;
+}
+const files: File[] = [
+  {
+    name: "index.html",
+    language: "html",
+    content: "<div id='app'>Hello World</div>",
+  },
+  {
+    name: "style.css",
+    language: "css",
+    content: "#app { color: blue; }",
+  },
+  {
+    name: "index.js",
+    language: "javascript",
+    content:
+      "console.log('Hello from JS!'); document.getElementById('app').style.fontSize = '24px';",
+  },
+];
 
 export default function Page() {
   return (
-    <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello World</h1>
-        <Button size="sm">Button</Button>
-      </div>
+    <div className="h-screen">
+      <Editor initialFiles={files} />
     </div>
-  )
+  );
 }
