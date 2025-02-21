c8aeef32b443e2671eda87319db4bd1f01b9d91b
Sachin Kumar <sachinkumar879162@gmail.com>
2025-02-21 19:32:33 +0530

    refactor: update env variables handling and cleanup code

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,19 +1,14 @@
 "use client";
 
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 
 import "@workspace/ui/globals.css";
 import { Providers } from "@/components/providers";
 import { CodeProvider } from "@/contexts/CodeContext";
 
-const fontSans = Geist({
+const fontInter = Inter({
   subsets: ["latin"],
-  variable: "--font-sans",
-});
-
-const fontMono = Geist_Mono({
-  subsets: ["latin"],
-  variable: "--font-mono",
+  variable: "--font-inter",
 });
 
 export default function RootLayout({
@@ -23,9 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${fontSans.variable} ${fontMono.variable} font-sans antialiased `}
-      >
+      <body className={`${fontInter.className} `}>
         <CodeProvider>
           <Providers>{children}</Providers>
         </CodeProvider>
