f232ee52a15c089d9e8cb3abcd2cdbb6f5c9e005
Sachin Kumar <sachinkumar879162@gmail.com>
2025-02-22 08:37:48 +0530

    fix: removed vm2

--- apps/web/lib/mongodb.ts
+++ apps/web/lib/mongodb.ts
@@ -1,11 +1,10 @@
-/* eslint-disable turbo/no-undeclared-env-vars */
 import mongoose from "mongoose";
 
 // Use environment variable for flexibility
 const DATABASE_URL = process.env.DATABASE_URL as string;
 
 if (!DATABASE_URL) {
-  throw new Error("No database url")
+  throw new Error("No database url");
 }
 
 const connectDB = async () => {
