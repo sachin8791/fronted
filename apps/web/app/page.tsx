312fb3cfe42eac1b7aa7c277fd5f90b7ef065802
Sachin Kumar <sachinkumar879162@gmail.com>
2025-02-23 17:15:07 +0530

    Changed page.tsx bg to white

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export default function Page() {
   return (
-    <div className="h-screen w-screen justify-center items-center flex flex-col gap-4">
+    <div className="h-screen w-screen justify-center items-center flex flex-col gap-4 bg-white">
       <p>Currently this Project is under dev Mode</p>
       <Link className="underline" href={"/questions"}>
         Go to Questions page
